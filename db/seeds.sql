a9c8f9397250df67795fd35eeee8000788cf6a32
James Howard <jth2698@gmail.com>
2020-10-17 22:40:40 -0500

    add JAWDB config

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,2 +1,3 @@
+INSERT INTO burgers (name) VALUES ('Hamburger');
 INSERT INTO burgers (name) VALUES ('Cheeseburger');
-INSERT INTO burgers (name, eaten) VALUES ('Hamburger', true);
+INSERT INTO burgers (name) VALUES ('Veggie Burger');
