d69b907fd4b7022c6ab8806628faf7b4eaaa380c
James Howard <jth2698@gmail.com>
2020-10-20 18:01:34 -0500

    remove specified port from connection.js

--- config/connection.js
+++ config/connection.js
@@ -7,7 +7,6 @@ if (process.env.JAWSDB_URL) {
 } else {
     connection = mysql.createConnection({
         host: "localhost",
-        port: 3306,
         user: "root",
         password: "JtH@888237384",
         database: "burger_db"
